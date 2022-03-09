6f9ed029da6d5c6e523009d8cd5e5e849bd94c65
Andrew <andrewjuang01@gmail.com>
2022-03-09 09:14:55 -0500

    started board.py

---
+++ app/board.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class Board:
+    def __init__(self, size, bombs):
+        self.size = size
+        self.bombs = bombs
+
+    def generate_board(size):
+        ''' Generates a board (numpy array) of a certain size '''
+        pass
+
+    def save_board(board):
+        ''' Saves the board to the database '''
+        pass
+
+    def get_boards(user):
+        ''' Gets the boards of a user '''
+        pass
+
+# b = Board(2,3)
+# print(b.bombs)
